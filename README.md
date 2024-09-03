3b764976474e1e2454e2f4772943ee5497a2f799
kykyxartemij <118753592+kykyxartemij@users.noreply.github.com>
2024-09-03 12:53:19 +0300

    learnexpressproject

---
+++ README.md
@@ -0,0 +1,6 @@
+How to open the exercise?
+Open Terminal in VS Code
+Download the module "express" - npm install express
+Copy and paste this command - npx sequelize-cli db:migrate
+Start the server - node server.js
+Open http://localhost:3000
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+const nunjucks = require("nunjucks");
+const db = require("./models/index");
+const paginate = require("./pagination");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
+
+// req => request , res => response
+app.get("/", async (req, res) => {
+  let page = req.query.page ? parseInt(req.query.page) : 1;
+  let perPage = 20;
+  let posts = await db.Post.findAll({
+    offset: (page - 1) * perPage,
+    limit: perPage,
+  });
+  let pages = Math.ceil((await db.Post.count()) / perPage);
+  let pagination = paginate(page, pages, perPage);
+  res.render("index.njk", { posts, pagination });
+});
+app.get("/answer", (req, res) => {
+  console.log(req.query);
+  res.render("answer.njk", req.query);
+});
+app.post("/answer", (req, res) => {
+  console.log(req.body);
+  res.render("answer.njk", { ...req.query, ...req.body });
+});
+
+app.use("/posts", require("./controllers/postController"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
